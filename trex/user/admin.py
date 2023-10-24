bbd0fb5dbbadab6114958f6e1c58381f62c52db1
aabid <115284244+aabidk20@users.noreply.github.com>
2023-10-24 15:43:11 +0530

    Set up admin panel for user detail view

--- trex/user/admin.py
+++ trex/user/admin.py
@@ -1,19 +1,72 @@
 from django.contrib import admin
-from .models import User
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+User = get_user_model()
+# from .models import User
 # Register your models here.
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
-        'id',
-        'username',
-        'first_name',
-        'last_name',
-        'role',
-        'email',
-        'updated_at',
+        "id",
+        "username",
+        "email",
+        "role",
+        "first_name",
+        "last_name",
     )
-
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    fieldsets = (
+        ('Auth', {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "avatar",
+                    "gender",
+                    "role",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                )
+            },
+        ),
+    )
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
 
