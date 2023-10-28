567f04ec58cbbdf471d693ca1a7fd6e8d0e759ba
aabid <115284244+aabidk20@users.noreply.github.com>
2023-10-28 06:20:44 +0530

    Made classroom description optional, and added unique together constraint for classroom and student

--- trex/classroom/models.py
+++ trex/classroom/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Classroom(models.Model):
     classroom_id = models.AutoField(primary_key=True)
     classroom_name = models.CharField(max_length=100, null=False)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     classroom_code = models.CharField(max_length=20, null=False)
     teacher = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='classrooms')
     created_on = models.DateTimeField(auto_now_add=True)
