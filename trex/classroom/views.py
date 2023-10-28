9cb8b6bc1bc2e2ec0ea70224e3c5d22b51350164
aabid <115284244+aabidk20@users.noreply.github.com>
2023-10-28 06:30:13 +0530

    Added searching and ordering filters; added filtered queryset and custom serializer as per user role for classroom list view

--- trex/classroom/views.py
+++ trex/classroom/views.py
@@ -10,15 +10,107 @@ from rest_framework.generics import (
     DestroyAPIView,
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter,
+)
+
 from .models import Classroom
-from .serializers import ClassroomSerializer, ClassroomListSerializer
+from .serializers import (
+    ClassroomSerializer,
+    ClassroomDetailSerializer,
+    StudentClassroomListSerializer,
+    TeacherClassroomListSerializer,
+)
+from trex.user.permissions import (
+    IsTeacher,
+    IsAdmin,
+    IsTeacherOfThisClassroom,
+)
 from core.utils import response_payload
 
 
 class ClassroomListView(ListAPIView):
-    # permission_classes = (IsAuthenticated,)
-    queryset = Classroom.objects.all()
-    serializer_class = ClassroomListSerializer
+    """
+    List all classrooms of the user.
+    User must be authenticated to access this view.
+
+    If user is a teacher, list all classrooms created by the teacher.
+    If user is a student, list all classrooms enrolled by the student.
+    Note that response format is different for teachers and students.
+
+    Searching is allowed on classroom_name, description, teacher__first_name, teacher__last_name
+    Ordering is allowed on classroom_name, teacher__first_name, created_on
+    Pagination is not supported yet.
+    """
+
+    permission_classes = [IsAuthenticated, ]
+
+    filter_backends = [
+        SearchFilter,
+        OrderingFilter
+    ]
+
+    search_fields = [
+        'classroom_name',
+        'description',
+        'teacher__first_name',
+        'teacher__last_name'
+    ]
+
+    ordering_fields = [
+        'classroom_name',
+        'teacher__first_name',
+        'created_on'
+    ]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'teacher':
+            queryset = Classroom.objects.filter(teacher=user)
+        elif user.role == 'student':
+            queryset = Classroom.objects.filter(enrollments__student=user)
+        elif user.is_superuser:
+            queryset = Classroom.objects.all()
+        else:
+            queryset = Classroom.objects.none()
+        return queryset
+
+    def get_serializer(self, *args, **kwargs):
+        """
+        Override get_serializer to return different serializer based on user role.
+        Using student serializer for students and teacher serializer for teachers or admins.
+        """
+        user = self.request.user
+        if user.role == 'teacher' or user.is_superuser:
+            return TeacherClassroomListSerializer(*args, **kwargs)
+        elif user.role == 'student':
+            return StudentClassroomListSerializer(*args, **kwargs)
+        else:
+            return super().get_serializer(*args, **kwargs)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        # return different response if no classrooms found
+        if len(serializer.data) == 0:
+            return Response(
+                response_payload(
+                    success=True,
+                    message="No classrooms found",
+                    data=serializer.data,
+                ),
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                response_payload(
+                    success=True,
+                    message="Classroom list fetched successfully",
+                    data=serializer.data,
+                ),
+                status=status.HTTP_200_OK,
+            )
 
 
 class ClassroomCreateView(CreateAPIView):
